dbea9df452356675bae15a77b58c1f77ed7922ee
vamayar <malkavian_va@hotmail.com>
2020-01-06 21:59:26 -0600

    Updates

---
+++ precache-manifest.9e6f36fab2cfbf232e355f72b791b014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31c6a353a328a26a89b0918baaadc29",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "38f7be6b460e4f278091",
+    "url": "/monsters-rolodex/static/css/main.d82f00af.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38f7be6b460e4f278091",
+    "url": "/monsters-rolodex/static/js/main.82065d76.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
